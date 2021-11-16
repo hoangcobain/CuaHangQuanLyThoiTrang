2cfe89d4f859d1e935bd52de252f18d78fd7885b
Development <70835318+Development707@users.noreply.github.com>
2021-11-16 21:04:43 +0700

    FIX TIM

--- src/main/java/com/Nhom05_DeTai01_PTUD_15A_2021/dao/HoaDonDAO.java
+++ src/main/java/com/Nhom05_DeTai01_PTUD_15A_2021/dao/HoaDonDAO.java
@@ -10,7 +10,7 @@ import com.Nhom05_DeTai01_PTUD_15A_2021.entity.HoaDon;
 
 public interface HoaDonDAO extends JpaRepository<HoaDon, String>{
 	
-	@Query(value = "SELECT * FROM [dbo].[hoa_don] WHERE ?1=[ma_nhan_vien] OR ?2=[ma_khach_hang] "
+	@Query(value = "SELECT * FROM [dbo].[hoa_don] WHERE ?1=[ma_nhan_vien] AND ?2=[ma_khach_hang] "
 			+ "OR (YEAR(ngay_lap_hoa_don)=?5 AND MONTH(ngay_lap_hoa_don) = ?4 AND DAY(ngay_lap_hoa_don) = ?3)",
 			nativeQuery = true)
 	List<HoaDon> searchHoaDonByNVKH(String maNV, String maKH, int day, int month, int year);
